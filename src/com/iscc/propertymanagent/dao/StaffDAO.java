f3b923920671c37eda0dc97334efc10b9c43e785
quguixian <1666013224@qq.com>
2020-08-06 19:02:55 +0800

    New StaffServlet.

--- src/com/iscc/propertymanagent/dao/StaffDAO.java
+++ src/com/iscc/propertymanagent/dao/StaffDAO.java
@@ -1,5 +1,11 @@
 package com.iscc.propertymanagent.dao;
 
-public interface StaffDAO {
+import com.iscc.propertymanagent.domain.Staff;
 
+import java.sql.ResultSet;
+
+public interface StaffDAO {
+    int addStaff(Staff staff);
+    ResultSet queryStaff();
+    int updateStaff(Staff staff);
 }
--- src/com/iscc/propertymanagent/dao/impl/StaffDaoImpl.java
+++ src/com/iscc/propertymanagent/dao/impl/StaffDaoImpl.java
@@ -1,4 +1,42 @@
 package com.iscc.propertymanagent.dao.impl;
 
-public class StaffDaoImpl {
+
+import com.iscc.propertymanagent.dao.StaffDAO;
+import com.iscc.propertymanagent.domain.Staff;
+import com.iscc.propertymanagent.util.DataSourceUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StaffDaoImpl implements StaffDAO {
+
+    @Override
+    public int addStaff(Staff staff) {
+        return 0;
+    }
+
+    @Override
+    public ResultSet queryStaff() {
+        String sql = "SELECT * FROM staff_info";
+        ResultSet result = null;
+        Connection conn = null;
+        PreparedStatement psmt = null;
+
+        try {
+            conn = DataSourceUtil.getConnection();
+            psmt = conn.prepareStatement(sql);
+            result = psmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result);
+        return  result;
+    }
+
+    @Override
+    public int updateStaff(Staff staff) {
+        return 0;
+    }
 }
